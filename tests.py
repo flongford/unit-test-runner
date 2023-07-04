d3db1d18ed1e311f1c24e83025013634c89583f6
flongford <flongford@enthought.com>
2023-07-04 18:18:27 +0100

    TST: iterate through multiple example modes

--- tests.py
+++ tests.py
@@ -8,11 +8,12 @@ class TestRunner(TestCase):
     """Boilerplate code to add tests to"""
 
     def test_change_file_permissions(self):
-        with TemporaryDirectory() as tmp_dir:
-            file_path = Path(tmp_dir) / "test.yaml"
-            file_path.write_bytes(b"")
-
-            expected_mode = 0o100660
-            os.chmod(str(file_path), expected_mode)
-
-            self.assertEqual(file_path.stat().st_mode, expected_mode)
+        for expected_mode in [0o100600, 0o100644, 0o100660]:
+            with self.subTest(expected_mode):
+                with TemporaryDirectory() as tmp_dir:
+                    file_path = Path(tmp_dir) / "test.yaml"
+                    file_path.write_bytes(b"")
+                    os.chmod(str(file_path), expected_mode)
+                    self.assertEqual(
+                        file_path.stat().st_mode, expected_mode
+                    )
