83490aa65e032ca3665019da530282d943246405
flongford <flongford@enthought.com>
2023-07-04 18:08:19 +0100

    TST: include file permissions unit test

--- tests.py
+++ tests.py
@@ -1,5 +1,18 @@
+import os
+from pathlib import Path
+from tempfile import TemporaryDirectory
 from unittest import TestCase
 
 
 class TestRunner(TestCase):
     """Boilerplate code to add tests to"""
+
+    def test_change_file_permissions(self):
+        with TemporaryDirectory() as tmp_dir:
+            file_path = Path(tmp_dir) / "test.yaml"
+            file_path.write_bytes(b"")
+
+            expected_mode = 0o100660
+            os.chmod(str(file_path), expected_mode)
+
+            self.assertTrue(file_path.stat().st_mode == expected_mode)
