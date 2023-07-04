42c98c86f70354846742b521f20c65d8fa2745c9
flongford <flongford@enthought.com>
2023-07-04 19:43:36 +0100

    TST: use mkstemp and remove touch tests

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import os
 import stat
 from pathlib import Path
-from tempfile import TemporaryDirectory
+from tempfile import mkstemp
 from unittest import TestCase
 
 
@@ -9,31 +9,22 @@ class TestRunner(TestCase):
     """Boilerplate code to add tests to"""
 
     def test_change_file_permissions_chmod(self):
-        for expected_mode in [0o666, 0o644, 0o600]:
-            with self.subTest(expected_mode):
-                with TemporaryDirectory() as tmp_dir:
-                    file_path = Path(tmp_dir) / f"{expected_mode}.yaml"
-                    file_path.touch()
+        for expected_mode in [0o620, 0o640, 0o660]:
+            with self.subTest(oct(expected_mode)):
+                tmp_file_fd, tmp_file_path = mkstemp(suffix="file.yaml")
+                os.close(tmp_file_fd)
+                tmp_file_path = Path(tmp_file_path)
 
-                    os.chmod(str(file_path), expected_mode)
+                actual_mode = stat.S_IMODE(tmp_file_path.stat().st_mode)
+                self.assertNotEqual(
+                    actual_mode, expected_mode,
+                    msg=f"{stat.filemode(actual_mode)} == {stat.filemode(expected_mode)}"
+                )
 
-                    actual_mode = stat.S_IMODE(file_path.stat().st_mode)
+                tmp_file_path.chmod(expected_mode)
 
-                    self.assertEqual(
-                        actual_mode, expected_mode,
-                        msg=f"{stat.filemode(actual_mode)} != {stat.filemode(expected_mode)}"
-                    )
-
-    def test_change_file_permissions_touch(self):
-        for expected_mode in [0o644, 0o600]:
-            with self.subTest(expected_mode):
-                with TemporaryDirectory() as tmp_dir:
-                    file_path = Path(tmp_dir) / f"{expected_mode}.yaml"
-                    file_path.touch(expected_mode)
-
-                    actual_mode = stat.S_IMODE(file_path.stat().st_mode)
-
-                    self.assertEqual(
-                        actual_mode, expected_mode,
-                        msg=f"{stat.filemode(actual_mode)} != {stat.filemode(expected_mode)}"
-                    )
+                actual_mode = stat.S_IMODE(tmp_file_path.stat().st_mode)
+                self.assertEqual(
+                    actual_mode, expected_mode,
+                    msg=f"{stat.filemode(actual_mode)} != {stat.filemode(expected_mode)}"
+                )
